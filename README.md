9a3f590ef99f8de276019eda26a16a2ff44063ec
gunjan1909 <bhanarkaranita@gmail.com>
2023-03-19 00:21:33 +0530

    first

---
+++ README.md
@@ -0,0 +1,7 @@
+## Simple single page portfolio template( Non responsive )
+
+Simple portfolio template in HTML, CSS and Javascript
+
+### PS: Need to make it responsive
+
+Using bootstrap's responsive navmenus and grid layout, or making other nav menus and using flexbox or css grid
---
+++ script.js
@@ -0,0 +1,25 @@
+let banner = document.querySelectorAll(".banner");
+let dayNight = document.querySelectorAll(".daynight");
+
+dayNight.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    banner.forEach((ele) => {
+      ele.classList.toggle("night");
+    });
+  });
+});
+
+let typingText = new Typed("#text", {
+  strings: ["Gunjan", "WebDev", "Coding", "Marvel Fan", "Music Addict"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+});
+let typingText2 = new Typed("#text2", {
+  strings: ["Gunjan", "WebDev", "Coding", "Marvel Fan", "Music Addict"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 1000,
+});
